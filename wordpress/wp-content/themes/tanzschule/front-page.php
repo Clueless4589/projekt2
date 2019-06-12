c2bb79e70812028bfc5211523f0a40b15cc4fb59
Sabrina <sabrina.zwirner@gmail.com>
2019-06-12 12:56:51 +0200

    styles update

--- wordpress/wp-content/themes/tanzschule/front-page.php
+++ wordpress/wp-content/themes/tanzschule/front-page.php
@@ -1,18 +1,28 @@
 <?php get_header();
 ?>
-<div class="col-12 col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
-    <h1> Dashboard</h1>
-    <div class="row">
-        <div class="col-12 col-lg-6">
-            <a href="<?php echo get_template_directory_uri()?>/lehrer-liste">
-                <div class="dashboard-button">Lehrer</div>
-            </a>
-        </div>
-        <div class="col-12 col-lg-6">
-            <a href="<?php echo get_template_directory_uri()?>/verwalter-uebersicht">
-                <div class="dashboard-button">Verwalter</div>
-            </a>
+<div class="main">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-10 col-md-10 col-xl-10">
+                <h1> Dashboard</h1>
+                <div class="row">
+                    <div class="col-12 col-lg-6">
+                       <a href="<?php echo get_template_directory_uri()?>/lehrer-liste">
+                           <div class="teaser lehrer">
+                             <p>Lehrer</p>
+                           </div>
+                       </a>
+                    </div>
+                    <div class="col-12 col-lg-6">
+                        <a href="<?php echo get_template_directory_uri()?>/verwalter-uebersicht">
+                            <div class="teaser manager">
+                                <p>Verwalter</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
-
 </div>
--- wordpress/wp-content/themes/tanzschule/page-lehrer-liste.php
+++ wordpress/wp-content/themes/tanzschule/page-lehrer-liste.php
@@ -1,49 +1,66 @@
 <?php get_header(); ?>
-    <div class="col-12 col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
-        <h1> Lehrerliste</h1>
-        <div class="row">
-            <div class="col-12 col-lg-6">
-                <label for="Name">Name</label>
-                <input type="text" name="Name" placeholder="Name suchen..." class="search-field">
+   <div class="main">
+       <div class="container-fluid">
+           <div class="row justify-content-center">
+               <div class="col-10 col-md-10 col-xl-10">
+                   <h1> Lehrerliste</h1>
+                   <div class="row">
+                       <div class="col-10 col-md-4">
+                          <div class="form-group">
+                              <label for="Name">Name</label>
+                              <input type="text" name="Name" placeholder="Name suchen..." class="search-field">
+                          </div>
 
-                <label for="Vorname">Vorname</label>
-                <input type="text" name="Vorname" placeholder="Vorname suchen..." class="search-field">
+                       </div>
+                       <div class="col-10 col-md-4">
+                           <div class="form-group">
+                               <label for="Vorname">Vorname</label>
+                               <input type="text" name="Vorname" placeholder="Vorname suchen..." class="search-field">
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <div class="row justify-content-center">
+               <div class="col-10 col-md-10 col-xl-10">
 
+                   <?php $liste = getLehrerliste() ?>
+                   <table>
+                       <thead>
+                       <tr>
+                           <th>Name</th>
+                           <th>Nachname</th>
+                           <th>Instrument</th>
+                           <th></th>
+                       </tr>
+                       </thead>
+                       <tbody>
+                       <!--foreach !!-->
+                       <?php foreach ($liste as $item) {
+                           $instrumente = "";
+                           foreach ($bezeichnungen = getInstrumente($item->LehrerId) as $bezeichnung) {
+                               $instrumente .= $bezeichnung->Bezeichnung . ", ";
+                           }
+                           $instrumente = rtrim($instrumente, ", ");
+                           ?>
 
-            </div>
+                           <tr>
+                               <td><?php echo $item->Vorname ?></td>
+                               <td><?php echo $item->Nachname ?></td>
+                               <td><?php
+                                   if(empty($instrumente)){
+                                       echo '-';
+                                   }
+                                   echo $instrumente;
 
-            <div class="col-12">
-
-                <?php $liste = getLehrerliste() ?>
-                <table>
-                    <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>Nachname</td>
-                        <td>Instrument</td>
-                        <td></td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <!--foreach !!-->
-                    <?php foreach ($liste as $item) {
-                        $instrumente = "";
-                        foreach ($bezeichnungen = getInstrumente($item->LehrerId) as $bezeichnung) {
-                            $instrumente .= $bezeichnung->Bezeichnung . ", ";
-                        }
-                        $instrumente = rtrim($instrumente, ", ");
-                        ?>
-
-                        <tr>
-                            <td><?php echo $item->Vorname ?></td>
-                            <td><?php echo $item->Nachname ?></td>
-                            <td><?php echo $instrumente ?></td>
-                            <td><a href="<?php echo get_template_directory_uri() ?>/lehrer-wochenplan?id=<?php echo $item->LehrerId ?>">Details</a></td>
-                        </tr>
-                    <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+                                   ?></td>
+                               <td><a href="<?php echo get_template_directory_uri() ?>/lehrer-wochenplan?id=<?php echo $item->LehrerId ?>">Zur Lehrerseite</a></td>
+                           </tr>
+                       <?php } ?>
+                   </tbody>
+                   </table>
+               </div>
+           </div>
+       </div>
+   </div>
 <?php
