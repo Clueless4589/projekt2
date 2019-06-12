1ebdcb41903882d07c74fa9b5b17e8323a8b9470
Sabrina <sabrina.zwirner@gmail.com>
2019-06-12 09:10:56 +0200

    update style paths for theme

--- wordpress/wp-content/themes/tanzschule/functions.php
+++ wordpress/wp-content/themes/tanzschule/functions.php
@@ -2,11 +2,10 @@
 function enquiIt()
 {
 
-    wp_register_style('style', get_template_directory_uri() . '/css/style.css');
+    wp_register_style('style', get_template_directory_uri() . '/dist/css/main.min.css');
     wp_enqueue_style('style');
 
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
-    wp_enqueue_style('bootstrap');
+
 }
 
 add_action( 'wp_enqueue_scripts', 'enquiIt' );
