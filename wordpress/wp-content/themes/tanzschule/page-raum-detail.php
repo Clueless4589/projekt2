b3a255f4197da5ab8fb88aa0f7fb8b5f1a0cb911
Clueless4589 <ma.muehlbauer@web.de>
2019-06-12 18:48:41 +0200

    TODO entfernt

--- wordpress/wp-content/themes/tanzschule/page-raum-detail.php
+++ wordpress/wp-content/themes/tanzschule/page-raum-detail.php
@@ -1,6 +1,5 @@
 <?php get_header();
-//TODO AJAX einfügen für die abfrage wann welcher Raum gelockt ist
-$zeiten = array("9:00","9:30","10:00","10:30", "11:00", "11.30","12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"); //TODO: schöner machen / was besseres einfallen lassen
+$zeiten = array("9:00","9:30","10:00","10:30", "11:00", "11.30","12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"); //TODO: schöner machen was besseres einfallen lassen
 $tage = array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag");
 
 ?>
