e1b20f3485516e331cf0f538162cb91d64de4a21
Clueless4589 <ma.muehlbauer@web.de>
2019-06-26 09:12:22 +0200

    anpassungen

--- wordpress/wp-content/themes/tanzschule/js/scripts.js
+++ wordpress/wp-content/themes/tanzschule/js/scripts.js
@@ -1,18 +1,22 @@
-jQuery.ajax({
-    type: "POST",
-    url: "/tanzschule/wordpress/wp-admin/admin-ajax.php",
-    data: ({
-        action: "getRaumKurse",
-        id : 2
-    }),
-    dataType: "json",
-    success: function (response){
-        console.log(response);
-        for ( i in response) {
-            console.log(this);
-            var time = response[i].Uhrzeit;
-            var day = response[i].Tag;
-            $('tr[data-time="' + time + '"] td.' + day + '').addClass('blocked')
-        }
+$(document).ready(function () {
+    if($('#raumdetail').length > 0){
+        jQuery.ajax({
+            type: "POST",
+            url: "/tanzschule/wordpress/wp-admin/admin-ajax.php",
+            data: ({
+                action: "getRaumKurse",
+                id : 2
+            }),
+            dataType: "json",
+            success: function (response){
+                console.log(response);
+                for ( i in response) {
+                    console.log(this);
+                    var time = response[i].Uhrzeit;
+                    var day = response[i].Tag;
+                    $('tr[data-time="' + time + '"] td.' + day + '').addClass('blocked')
+                }
+            }
+        });
     }
 });
