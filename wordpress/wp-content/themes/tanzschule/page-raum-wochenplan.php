50eedddb2d4cb2fed70819eb4d6b98d81ab35b72
Sabrina <sabrina.zwirner@gmail.com>
2019-06-26 09:14:38 +0200

    iso fix

--- wordpress/wp-content/themes/tanzschule/page-raum-wochenplan.php
+++ wordpress/wp-content/themes/tanzschule/page-raum-wochenplan.php
@@ -8,53 +8,28 @@ $raum= getAllRaum();
             <div class="row justify-content-center">
                 <div class="col-10 col-md-10 col-xl-10">
                     <h1>Raum Wocheplan</h1>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    <hr>
                     <div class="row">
-                        <div class="button-group filters-button-group">
-                            <button class="button is-checked" value="Alle Verhütungsmethoden" data-filter="*">
-                                <span>Alle</span>
-                            </button>
-
-                            <?php
-
-                            foreach ($instrumente as $instrument) {
-                                $bzg = $instrument->Bezeichnung;
-                                ?>
-                                <button class="button" data-filter=".<?php echo $bzg?>" value="<?php echo $bzg?>">
-                                    <span><?php echo $bzg?></span></button>
+                        <div class="col-10">
+                            <div id="filters" class="button-group">
+                                <button class="btn is-checked" data-filter="*">show all</button>
                                 <?php
-                            }?>
+
+                                foreach ($instrumente as $instrument) {
+                                    $bzg = $instrument->Bezeichnung;
+                                    ?>
+                                    <button class="btn" data-filter=".<?php echo $bzg?>">
+                                        <span><?php echo $bzg?></span></button>
+                                    <?php
+                                }?>
+                            </div>
                         </div>
                     </div>
-                    <div class="row grid justify-content-center">
+
+                    <div class="row grid mt-5">
+
+
+
 
                         <?php
                         foreach ($raum as $raumitem) {
@@ -62,14 +37,18 @@ $raum= getAllRaum();
                             $raumname= $raumitem->Bezeichnung;
                             $rauminstrumente= getRaumInstrumente($id);
                             ?>
-                            <div class="col-lg-3 col-sm-4 col-6 element-item <?php foreach ($rauminstrumente as $rauminstrument) {
+                            <div class="col-lg-2 col-sm-3 col-5 element-item <?php foreach ($rauminstrumente as $rauminstrument) {
                                 $instrument= $rauminstrument->Instrument;
                                 echo $instrument.' ';
-                            }?>" data-category="transition">
+                            }?>">
                                 <div class="element-item-container">
-                                    <h2>
-                                        <?php echo $raumname?>
-                                    </h2>
+                                        <p>
+                                            <a href="<?php echo get_template_directory_uri() ?>/raum-detail?id=<?php echo $id ?>">
+
+                                            <?php echo $raumname?>
+                                            </a>
+                                        </p>
+
                                 </div>
                             </div>
 
